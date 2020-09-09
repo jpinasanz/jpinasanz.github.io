0ac00a16cc668904d1b8c5b43a1deb87d89dc7b1
jpinasanz <jpinasanz@sdsu.edu>
2020-09-08 19:55:47 -0700

    updated navbar

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function() {
+    //Insert HTML into 'selector' 
+    var insertHtml = function(selector, html) {
+        var targetElem = document.querySelector(selector);
+        targetElem.innerHTML = html;
+    };
+
+    function buildHTML() {
+
+    }
+
+})(window);
